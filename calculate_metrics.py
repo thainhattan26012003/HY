6ea43cd05dd1d6ead980b78272326ab544d6d23b
thainhattan26012003 <thainhattan26012003@gmail.com>
2024-06-12 11:54:33 +0700

    saving_weight for 128,256,512,64,64

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -2,32 +2,16 @@ import csv
 import numpy as np
 import pandas as pd
 
-# Load data from CSV file
-filename = "Trinh_ngu.csv"
-
-# Initialize lists to store data
-data = []
-
-# Read data from CSV file
-with open(filename, 'r') as file:
-    csv_reader = csv.reader(file)
-    for row in csv_reader:
-        data.append(row)
-
-# Convert data to numpy array for calculations
-data_array = np.array(data)
-
-# Remove non-numeric columns
-numeric_data = data_array[:, 1:7].astype(float)
-
-# Calculate mean for each column
-column_means = np.mean(numeric_data, axis=0)
-
-# Print the mean for each column
-print("Mean for each column:")
-print(column_means)
-
 
 df = pd.read_csv('Trinh_ngu.csv')
-a = df.iloc[:, 1:2].values.tolist()
-print(np.mean(a))
+
+results_lst = []
+for i in range(1,7):
+    a = df.iloc[:,i:i+1].values.tolist()
+    results_lst.append(np.mean(a))
+    
+column_names = ["Similarity", "MAE", "RMSE", "FSD", "R_score", "NSE"]
+
+df_named = pd.DataFrame([results_lst], columns=column_names)
+
+print(df_named)
