1e406ea05b432bdea9e3a1ae9cbe1227a2b42cc4
thainhattan26012003 <thainhattan26012003@gmail.com>
2024-06-13 07:36:26 +0700

    done 3rd update weights

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -7,7 +7,7 @@ df = pd.read_csv('Trinh_ngu.csv')
 
 results_lst = []
 for i in range(1,7):
-    a = df.iloc[:,i:i+1].values.tolist()
+    a = df.iloc[:,i:i+1].values
     results_lst.append(np.mean(a))
     
 column_names = ["Similarity", "MAE", "RMSE", "FSD", "R_score", "NSE"]
